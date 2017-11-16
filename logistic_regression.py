504b94853622480cbd31c65b9ed04ddddd0fbae1
Chuanye "Elvin" Ouyang <elvin.ouyang@gmail.com>
2017-11-16 13:01:39 -0500

    updated logistic regression to increase fraud sample size

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,30 +10,39 @@ file_name = '../creditcard.csv'
 rng = np.random.RandomState(1122)
 
 raw_data = pd.read_csv(file_name)
-x_train, x_test, y_train, y_test = train_test_split(
-    raw_data.iloc[:, :-1].as_matrix(),
-    raw_data.iloc[:, -1].values.astype(str),
-    random_state=rng, test_size=0.1)
 
+fraud_case = raw_data[raw_data['Class'] == 1]
+print('There are %i fraudulent cases in the dataset...' % len(fraud_case))
+genuine_case = raw_data[raw_data['Class'] == 0]
+print('There are %i genuine cases in the dataset...' % len(genuine_case))
+
+print('The data target class is highly unbalanced...')
+print('Generating a balanced sample with roughly 1:1 for fraud:genuine...')
+adjusted_raw = fraud_case.append(genuine_case.sample(n=2460, random_state=rng))
+
+x_train, x_test, y_train, y_test = train_test_split(
+    adjusted_raw.iloc[:, :-1].as_matrix(),
+    adjusted_raw.iloc[:, -1].values.astype(str),
+    random_state=rng, test_size=0.25)
+print('Training and testing set splited...')
 print('Data loaded...')
 
 logfraud = LogisticRegression(
     penalty='l2',
-    class_weight={'1': 1000, '0': 1},
     random_state=rng,
+    class_weight={"1": 5, "0": 1},
     solver='liblinear')
-
 logfraud.fit(x_train, y_train)
 print('Model trained...')
 
-y_pred = logfraud.predict(x_test)
-y_pred[y_pred == '0'] = 0
-y_pred[y_pred == '1'] = 1
-print('Prediction made...')
+y_pred = logfraud.predict(x_test).astype(int)
+print('Predictions made...')
+
+y_test = y_test.astype(int)
 
 # Evaluation 1: Scores
 cm = confusion_matrix(y_test, y_pred)
-prec = precision_score(y_test, y_pred)  # TODO: debug this line
+prec = precision_score(y_test, y_pred)
 print("Model prediction's confusion matrix:")
 print(cm)
 print("Model achieved precision score of %.2f percent" % (prec * 100))
